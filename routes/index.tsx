ec217a5e6422d23346b58391a6f89257c5f3da42
Jeje <140772578+4wre@users.noreply.github.com>
2024-12-21 05:19:50 +0000

    admins login handler

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,19 @@
 import { define } from "~/utils/core.ts";
 import { Deploy } from "~/islands/deploy.tsx";
+import { page } from "fresh";
+import { createAuthUrl } from "~/utils/auth.ts";
 
-export default define.page((_ctx) => {
+export const handler = define.handlers({
+	GET(ctx) {
+		if (!ctx.state.member) {
+			return ctx.redirect(createAuthUrl());
+		} else {
+			return page();
+		}
+	},
+});
+
+export default define.page<typeof handler>((_ctx) => {
 	return (
 		<div class="flex h-dvh p-2">
 			<Deploy />
