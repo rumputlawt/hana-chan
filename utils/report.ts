418c9f0c5e153af230a87e6b3306f2c5d054b626
sera <serotoninoy@gmail.com>
2024-12-24 03:48:13 +0000

    prevent self-report

--- utils/report.ts
+++ utils/report.ts
@@ -35,56 +35,65 @@ export async function reportMessage(
 
 		const { author, content, embeds, attachments, poll, id: messageId } =
 			retrieveResolvedMessage(interaction);
-		const report = await bot.webhooks.execute(
-			botWebhook.id,
-			botWebhook.token!,
-			{
-				allowed_mentions: {
-					parse: [],
-				},
-				attachments,
-				avatar_url: avatar(author),
-				content: content.trim(),
-				embeds,
-				poll,
-				thread_name:
-					`A message has been reported @ ${interaction.channel.name}`,
-				username: author.username,
-				wait: true,
-			},
-		);
 
-		const moderatorRole = Deno.env.get("MODERATOR_ROLE_ID");
-		await bot.channels.createMessage(report.channel_id, {
-			content: `⚠️ A message has been reported! ${
-				moderatorRole ? roleMention(moderatorRole) : ""
-			}`,
-			embeds: [{
-				color: 0xA2D1FE,
-				author: {
-					name: interaction.member!.user.username,
-					icon_url: avatar(interaction.member!.user),
+		if (interaction.member?.user.id === author.id) {
+			await bot.interactions.editReply(
+				interaction.application_id,
+				interaction.token,
+				{ content: "You can't report yourself yk?" },
+			);
+		} else {
+			const report = await bot.webhooks.execute(
+				botWebhook.id,
+				botWebhook.token!,
+				{
+					allowed_mentions: {
+						parse: [],
+					},
+					attachments,
+					avatar_url: avatar(author),
+					content: content.trim(),
+					embeds,
+					poll,
+					thread_name:
+						`A message has been reported @ ${interaction.channel.name}`,
+					username: author.username,
+					wait: true,
 				},
-				description: `Chnanel: ${
-					channelMention(interaction.channel.id)
-				}\n\nReported by: ${
-					userMention(interaction.member!.user.id)
-				}\nReported user: ${userMention(author.id)}`,
-			}],
-			components: [{
-				type: ComponentType.ActionRow,
-				components: [{
-					type: ComponentType.Button,
-					style: ButtonStyle.Link,
-					label: "Jump to message",
-					url: messageLink(interaction.channel.id, messageId),
+			);
+
+			const moderatorRole = Deno.env.get("MODERATOR_ROLE_ID");
+			await bot.channels.createMessage(report.channel_id, {
+				content: `⚠️ A message has been reported! ${
+					moderatorRole ? roleMention(moderatorRole) : ""
+				}`,
+				embeds: [{
+					color: 0xA2D1FE,
+					author: {
+						name: interaction.member!.user.username,
+						icon_url: avatar(interaction.member!.user),
+					},
+					description: `Chnanel: ${
+						channelMention(interaction.channel.id)
+					}\n\nReported by: ${
+						userMention(interaction.member!.user.id)
+					}\nReported user: ${userMention(author.id)}`,
 				}],
-			}],
-		});
-		await bot.interactions.editReply(
-			interaction.application_id,
-			interaction.token,
-			{ content: "Your report has been recorded." },
-		);
+				components: [{
+					type: ComponentType.ActionRow,
+					components: [{
+						type: ComponentType.Button,
+						style: ButtonStyle.Link,
+						label: "Jump to message",
+						url: messageLink(interaction.channel.id, messageId),
+					}],
+				}],
+			});
+			await bot.interactions.editReply(
+				interaction.application_id,
+				interaction.token,
+				{ content: "Your report has been recorded." },
+			);
+		}
 	}
 }
